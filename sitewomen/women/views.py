51786fadecbcbc8b9d13981867ee42964bb91572
Yugenyi Busygin <abusyginza@gmail.com>
2025-01-06 19:01:03 +0700

    redirect

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 
 def index(request):
@@ -12,6 +13,9 @@ def categories_by_slug(request, cat_slug):
     return HttpResponse(f'<h2> Категории </h2><p>slug: {cat_slug}</p>')
 
 def archive(request, year):
+    if year > 2023:
+        uri = reverse('cats', args=('music', ))
+        return HttpResponseRedirect('/')
     return HttpResponse(f'<h2> Архив по годам </h2><p>{year}</p>')
 
 def page_not_found(request, exception):
