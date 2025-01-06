db4cde2e6cbe81f38551e27f734e3ee4a185ab7e
Yugenyi Busygin <abusyginza@gmail.com>
2025-01-06 08:41:53 +0700

    views

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('<h1> Привет мир </h1>')
+
+def categories(request):
+    return HttpResponse('<h3> Категории </h3>')
+
